6efd9530112073117376bc39cc061b72a79dc2c1
Yuriy Tigunov <192215@gmail.com>
2022-08-23 10:07:04 +0800

    lesson 2. Buble sort and quick sort

---
+++ app/Handlers/Sort/SortInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Handlers\Sort;
+
+interface SortInterface
+{
+    function sortArr($arrayay): array;
+
+}
--- app/Http/Controllers/SortController.php
+++ app/Http/Controllers/SortController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\Sort\BubleSort;
+use App\Handlers\Sort\QuickSort;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -15,12 +17,25 @@ class SortController extends Controller
 
         $array = [1, 3, 6, 100, 4, 50, 40, 39, 38];
 
-        sort($array);
+//        sort($array);
+
+        $buble_sort = new BubleSort();
+        $buble_sort_array = $buble_sort->sortArr($array);
+
+        $quick_sort = new QuickSort();
+        $quick_sort_array = $quick_sort->sortArr($array);
 
         $memory_usage = memory_get_usage();
         $time_end = time();
         Log::info('Закончил работу в ' . $time_end);
         Log::debug('Использовано памяти: ' . $memory_usage);
-        dd($array);
+
+        echo 'Быстрая сортировка: ';
+        var_dump($quick_sort_array);
+
+        echo 'Сортировка пузырьком: ';
+
+        var_dump($buble_sort_array);
     }
+
 }
